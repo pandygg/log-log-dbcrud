82eb4c72dda744bb2747dc04b581ef2a065e071d
pandygg <egapandy@gmail.com>
2024-09-16 18:27:35 -0700

    Add files via upload

---
+++ Login and Register/Database/dbcrud2022 (2).sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Sep 2024 pada 10.09
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcrud2022`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbarang`
+--
+
+CREATE TABLE `tbarang` (
+  `id_barang` int(11) NOT NULL,
+  `kode` varchar(15) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `asal` varchar(25) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `satuan` varchar(15) NOT NULL,
+  `tanggal_diterima` date NOT NULL,
+  `tanggal_simpan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tbarang`
+--
+
+INSERT INTO `tbarang` (`id_barang`, `kode`, `nama`, `asal`, `jumlah`, `satuan`, `tanggal_diterima`, `tanggal_simpan`) VALUES
+(3, 'INV-2022-003', 'laptop rog', 'sumbangan', 1, 'unit', '2022-06-04', '2024-08-19 04:27:25'),
+(11, 'INV-2024-004', 'helo', 'Hibah', 2, 'Kotak', '2024-09-25', '2024-09-02 05:06:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbarang`
+--
+ALTER TABLE `tbarang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbarang`
+--
+ALTER TABLE `tbarang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
